fa207cf12a201acd4d46d221783ffa786497b60d
xh1021 <xh1021@vip.qq.com>
2015-12-05 23:21:56 +0800

    cooper

--- static/script-ss/page/back.js
+++ static/script-ss/page/back.js
@@ -1,20 +1,8 @@
-var app = require('back/app.vue')
-
 Vue.config.debug = true
 
-var pages = [
-	{
-		items: [
-			{ message: '1111' },
-			{ message: '2222' }
-		]
-	},{
-		items: [
-			{ message: '3333' },
-			{ message: '4444' }
-		]
-	}	
-]
+var app = require('back/app.vue')
+
+var pages = require('data/pages')
 var number = 0
 
 var vm = new Vue({
@@ -29,4 +17,6 @@ var vm = new Vue({
 })
 
 console.log(vm)
+console.log(pages)
+
 
