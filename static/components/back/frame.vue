4e1a22224793e99d8fe13a3e5778556ef9000cba
xh1021 <xh1021@vip.qq.com>
2015-12-08 17:18:48 +0800

    vue

--- static/components/back/frame.vue
+++ static/components/back/frame.vue
@@ -2,6 +2,7 @@
 .frame{
 	.w(12px);.h(24px);
 	.left;
+	.pr;
 	.bgc(#333);
 	border:1px solid #222;
 	.ml(-1px);.mb(-1px);
@@ -9,15 +10,18 @@
 	&:nth-child(5n+1){
 		.bgc(#3a3a3a);
 	}
-
-	.focus{
-
+	
+}
+.keyframe{
+	&:before{
+		content:'';.pa;top:50%;left:50%;.w(6px);.h(6px);.mt(-3px);.ml(-3px);.bgc(#000);
+		.border-r(8px);
 	}
 }
 </style>
 
 <template>
-<div class="frame" @click.stop.prevent="setFocus"></div>
+<div class="frame" :class="framedata ? (framedata.type || 'keyframe') : ''" @click.stop.prevent="setFocus"></div>
 </template>
 
 
