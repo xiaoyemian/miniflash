c54991d957871d42a32c1fce95e310e4e623ad7d
许 慧 <xh1021@vip.qq.com>
2015-12-10 21:58:40 +0800

    view

--- static/components/back/app.vue
+++ static/components/back/app.vue
@@ -20,49 +20,118 @@ body{
 <template>
 <div class="app">
 	<div class="headtop"></div>
-	<view v-ref:view :pagedata="pages[number]" :pagesize="pagesize" :flashsize="flashsize" :viewsize="viewsize"></view>
+
+	<flash :style="flashstyle" :tracksdata="pagedata.tracks" :focus="focus"></flash>
+
+	<view :focus="focus" :pagedata="pagedata" :pagesize="pagesize" :pagestyle="pagestyle" :viewstyle="viewstyle" :itemsettingstyle="itemsettingstyle"></view>
+
 </div>
 </template>
 
 
 <script>
+require('jquery')
+require('jqui/draggable')
+require('jqui/resizable')
+
+var flash = require('back/flash.vue')
 var view = require('back/view.vue')
 
 
 return {
   components: {
-		view:view
+		flash : flash
+		, view : view
   }
+	, props:['pages','number']
 	, data : function(){
 		return {
-			printsize : {}	
+			pagedata : this.pages[this.number]
+			, focus : {}
+			, printsize : {}	
 			, flashsize : {}
 			, viewsize : {} 
 			, pagesize : {}
+			
+			, flashstyle : {}
+			, viewstyle : {}
+			, itemsettingstyle : {}
+			, pagestyle : {}
 		}
 	}
-	, props:['pages','number']
 	, methods : {
-		clearFocus : function(){
-			this.$broadcast('clearFocus')
-		}
 	}
 	, events : {
-		updateFlash : function(height){
+    setFocus : function(item_id, frame_id){
+      if(item_id == this.focus.item_id && frame_id == this.focus.frame_id)
+        return;
+
+      if(typeof frame_id == 'undefined'){
+        frame_id = this.focus[item_id]|0 + ''
+        this.$broadcast('selectFrame', item_id, frame_id)
+
+      }else{
+        frame_id = frame_id|0 + ''
+        Vue.set(this.focus, 'item_id', item_id)
+        Vue.set(this.focus, 'frame_id', frame_id)
+
+        Vue.set(this.focus, 'style', this.pagedata.tracks[item_id][frame_id].style)
+        Vue.set(this.focus, item_id, frame_id)
+
+        this.$emit('updataItemStyle')
+      }
+    }
+
+		, updataItemStyle : function(style, item_id){
+      if(!style)
+        style = this.focus.style
+
+      if(style['padding-top']){
+        var pagesize = this.pagesize
+
+        Vue.set(style, 'height', pagesize.width * style['padding-top']/100)
+
+        style.width = pagesize.width * style['width']/100
+        style.top = pagesize.height * style['top']/100
+        style.left = pagesize.width * style['left']/100
+
+        delete style['padding-top']
+      }
+
+      this.$broadcast('updateItem', {style:style, item_id:item_id})
+    }
+
+		, clearFocus : function(){
+      Vue.set(this.focus, 'item_id', null)
+      Vue.set(this.focus, 'frame_id', null)
+      Vue.set(this.focus, 'style', {})
+    }
+
+
+		, updateFlash : function(height){
 			Vue.set(this.flashsize, 'height', height)
-			this.$broadcast('updateAppView')
+
+      Vue.set(this.flashstyle, 'height', this.flashsize.height + 'px')
+
+      Vue.set(this.viewstyle, 'top', this.flashsize.height + 'px')
+      Vue.set(this.itemsettingstyle, 'top', this.flashsize.height + 'px')
+
 		}
 		, updatePage : function(width, height){
 			width = width || 640
 			height = height || 1136
+
 			Vue.set(this.printsize, 'width', width)
 			Vue.set(this.printsize, 'height', height)
 			
 			Vue.set(this.pagesize, 'width', this.printsize.width * 0.4)
 			Vue.set(this.pagesize, 'height', this.printsize.height * 0.4)
 
-			this.$broadcast('updateAppView')
-		}
+			Vue.set(this.pagestyle, 'width', this.pagesize.width + 'px')
+			Vue.set(this.pagestyle, 'height', this.pagesize.height + 'px')
+			Vue.set(this.pagestyle, 'margin-top', this.pagesize.height/-2 + 'px')
+			Vue.set(this.pagestyle, 'margin-left', this.pagesize.width/-2 + 'px')
+    }
 	}
 	, created: function(){
 		this.$emit('updatePage', 640, 1136)
--- static/components/back/view.vue
+++ static/components/back/view.vue
@@ -11,14 +11,14 @@
 	.page{
 		.pa;
 		.bgc(#fff);
+		left:50%;
+		top:50%;
 	}
 }
 </style>
 
 
 <template>
-<flash :style="flashstyle" :tracksdata="pagedata.tracks" :focus="focus"></flash>
-
 <itemsetting :focus="focus" :itemdata="pagedata.items[focus.item_id]" :itemsettingstyle="itemsettingstyle"></itemsetting>
 
 <div class="view" @click="clearFocus" :style="viewstyle">
@@ -31,107 +31,25 @@
 
 
 <script>
-require('jquery')
-require('jqui/draggable')
-require('jqui/resizable')
-
 var item = require('back/item.vue')
 var itemsetting = require('back/itemsetting.vue')
-var flash = require('back/flash.vue')
 
 return {
   components : {
     item : item
-		, flash : flash 
 		, itemsetting : itemsetting
   }
-	, props:['pagedata', 'pagesize', 'flashsize', 'viewsize']
+	, props:['focus', 'pagedata', 'pagesize', 'pagestyle', 'viewstyle', 'itemsettingstyle']
 	, data : function(){
-
 		return {
-			focus : {} 
-			, pagestyle : {}
-			, itemsettingstyle : {} 
-			, flashstyle : {}
-			, viewstyle : {}
-		}
-	}
-	, watch : {
-		'focus' : function(){
-			console.log(arguments)
 		}
 	}
 	, methods : {
 		clearFocus : function(){
-			Vue.set(this.focus, 'item_id', null)
-			Vue.set(this.focus, 'frame_id', null)
-			Vue.set(this.focus, 'style', {})
+			this.$dispatch('clearFocus')
 		}
 	}
 	, events : {
-		setFocus : function(item_id, frame_id){
-			if(item_id == this.focus.item_id && frame_id == this.focus.frame_id)
-				return;
-
-			if(typeof frame_id == 'undefined'){
-				frame_id = this.focus[item_id]|0 + ''
-				this.$broadcast('selectFrame', item_id, frame_id)
-
-			}else{
-				frame_id = frame_id|0 + ''
-				Vue.set(this.focus, 'item_id', item_id)
-				Vue.set(this.focus, 'frame_id', frame_id)
-
-				Vue.set(this.focus, 'style', this.pagedata.tracks[item_id][frame_id].style)
-				Vue.set(this.focus, item_id, frame_id)
-
-				this.$emit('updataItemStyle')
-			}
-		}
-		, updataItemStyle : function(style, item_id){
-			if(!style)
-				style = this.focus.style
-
-			if(style['padding-top']){
-				var pagesize = this.pagesize
-
-				Vue.set(style, 'height', pagesize.width * style['padding-top']/100)
-
-				style.width = pagesize.width * style['width']/100
-				style.top = pagesize.height * style['top']/100
-				style.left = pagesize.width * style['left']/100
-
-				delete style['padding-top']
-			}
-
-			this.$broadcast('updateItem', {style:style, item_id:item_id})
-		}
-		, clearFocus : function(){
-			this.clearFocus()
-		}
-		, updateAppView : function(){
-			this.pagestyle = {
-				width : this.pagesize.width + 'px'
-				, height : this.pagesize.height + 'px'
-				, left : '50%'
-				, top : '50%'
-				, 'margin-top' : this.pagesize.height/-2 + 'px'
-				, 'margin-left' : this.pagesize.width/-2 + 'px'
-			}
-			this.itemsettingstyle = {
-				top : this.flashsize.height + 'px'
-			}
-			this.flashstyle = {
-				height : this.flashsize.height + 'px'
-			}
-			this.viewstyle = {
-				height : $('.app').height() - this.flashsize.height + 'px'
-			}
-		}
-	}
-	, ready : function(){
-		this.$emit('updateAppView')
-
 	}
 }
 </script>
