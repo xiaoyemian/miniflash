e9e714d1de4fe7be15b76b57a053929440371c44
xh1021 <xh1021@vip.qq.com>
2015-12-12 02:40:19 +0800

    item

--- static/components/back/itemsetting.vue
+++ static/components/back/itemsetting.vue
@@ -1,45 +1,31 @@
 <style>
 .itemsetting{
 	z-index:900;
-	.pa;
-	right:0px;
-
-	.w(130px);
-	padding:0px 10px 10px 10px;
+	.pa; right:0px; .w(130px); padding:0px 10px 10px 10px;
 
 	&>div{.hidden;.mt(10px);padding:2px;}
 
 	.inputBox{
-		.f(12px);.fc(#ccc);
-		padding:2px;
-		.l(20px);
-		.left;
+		.f(12px);.fc(#ccc); padding:2px; .l(20px); .left;
 	}
 	input{
 		&[type="text"],&[type="number"]{
 			.i_block;.fc(#ccc);.bgc(#383838);padding:2px 6px;
 			border:1px solid #2e2e2e;
 		}
-		&[type="text"]{
-      .w(100px);
-    }
-    &[type="number"]{
-      .c;.w(44px);
-    }		
+		&[type="text"]{ .w(100px); }
+    &[type="number"]{ .c;.w(44px); }		
 	}
 	label {
 		.i_block; .tr;
 	}
 }
 
-
 .itemStyle{
-	.bgc(#333);
-	border:1px solid #222;
+	.bgc(#333); border:1px solid #222;
 }
 .itemType{
-	.bgc(#333);
-	border:1px solid #222;
+	.bgc(#333); border:1px solid #222;
 }
 </style>
 
@@ -50,8 +36,8 @@
 		<div class="inputBox" v-for="(key, value) in label.style"><label for="style|{{key}}">{{value}}</label><input type="number" @keyup="updateItem" id="style|{{key}}" placeholder="" value="{{focus_item.framestyle[key]}}"/>px</div>
 	</div>
 
-	<div class="itemType" v-if="0 && itemdata.background">
-		<div class="inputBox" v-for="(key, value) in label.background"><label for="background|{{key}}">{{value}}</label><input type="text" id="background|{{key}}" placeholder="" value="{{itemdata.background[key]}}"/></div>
+	<div class="itemType" v-if="focus_item.itemdata.background">
+		<div class="inputBox" v-for="(key, value) in label.background"><label for="background|{{key}}">{{value}}</label><input type="text" id="background|{{key}}" placeholder="" value="{{focus_item.itemdata.background[key]}}"/></div>
 
 	</div>
 </div>
