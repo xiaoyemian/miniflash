8d38c60db9acef22c92473d05f16043bf75a514f
xh1021 <xh1021@vip.qq.com>
2015-12-22 19:27:25 +0800

    frame

---
+++ static/components/flash/back/settings/framesetting.vue
@@ -0,0 +1,48 @@
+<style>
+</style>
+
+<template>
+<div class="settingBox" v-if="focus_item">
+	<div class="inputArea" v-for="(key, value) in formatdata.resize">
+		<label for="resize|{{key}}">{{value.label || key}}:</label>
+		<input type="{{value.type||'number'}}" @keydown="updateItem" id="resize|{{key}}" placeholder="" value="{{focus_item.framedata.resize[key]}}"/>{{value.unit||''}}
+	</div>
+
+	<div class="inputArea" v-for="(key, transform) in formatdata.transform">
+		<div class="inputLabel">{{transform.label || key}}:</div>
+		<div class="inputBox" v-for="value in transform.opts">
+			<label for="transform|{{key}}|{{value[0]}}">{{value[0]}}:</label>
+			<input type="number" step="{{value[3]}}" @keydown="updateItem" id="transform|{{key}}|{{value[0]}}" placeholder="" value="{{ focus_item.framedata.transform[key][value[0]] }}"/>{{value[1]||''}}
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+
+return {
+	props : ['focus_item', 'formatdata']
+	, data : function(){
+		return {
+		}
+	}
+	, methods : {
+		updateItem : function(event){
+			var setting = event.target
+			var type = setting.id.split('|')
+			var value = setting.value
+
+			var arr = []
+			for(var i in type){
+				arr.push('["' + type[i] + '"]')
+			}
+
+			this.$set('focus_item.framedata' + arr.join(''), value||0)
+			console.log(this.focus_item.framedata, value||0)
+
+			this.focus_item.loadItemStyle()
+		}
+	}
+}
+</script>
+
