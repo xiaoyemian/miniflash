d659e00129afcfde4b9686be61fa19c138d7c266
xh1021 <xh1021@vip.qq.com>
2015-12-28 14:51:53 +0800

    setting

---
+++ static/components/flash/back/settings/flash.vue
@@ -0,0 +1,24 @@
+<style>
+</style>
+
+<template>
+<div class="settingFlash" v-if="focus_frame">
+	<div class="inputArea" v-for="(key, value) in formatdata">
+		<label for=""></label>
+	</div>
+</div>
+</template>
+
+<script>
+
+return {
+	props : ['focus_frame', 'formatdata']
+	, data : function(){
+		return {
+		}
+	}
+	, methods : {
+	}
+}
+</script>
+
