c9ce95a0c8137968a7058449a942614048119a66
xh1021 <xh1021@vip.qq.com>
2016-01-10 03:28:27 +0800

    frame

--- static/components/flash/back/controls/block.vue
+++ static/components/flash/back/controls/block.vue
@@ -4,10 +4,10 @@
 	<div @click.stop="changeBlock2Transition" class="transitionBox" v-if="global.item.track.block.blockdata.name != 'transition'">转换为过渡动画</div>
 	<div @click.stop="changeBlock2Animation" class="animationBox" v-if="global.item.track.block.blockdata.name != 'animation'">转换为预设动画</div>
 
-	<div @click.stop="removeBlock" class="itemBox" v-if="global.item && global.item.itemdata.blocks.length > 1">删除动作</div>
-
 	<div @click.stop="addFrame" class="frameBox" v-if="global.item.track.block.blockdata.name == 'transition'">添加帧</div>
 	<div @click.stop="removeFrame" class="frameBox" v-if="global.item.track.block.blockdata.name == 'transition' && global.item.track.block.blockdata.frames.length > 1">删除帧</div>
+
+	<div @click.stop="removeBlock" class="itemBox" v-if="global.item && global.item.itemdata.blocks.length > 1">删除动作</div>
 </div>
 
 </template>
