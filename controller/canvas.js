8b1a5143d478c0ffc2c7957d95dc57d14332bcaa
xh1021 <xh1021@vip.qq.com>
2016-07-17 07:39:45 +0800

    view

--- controller/canvas.js
+++ controller/canvas.js
@@ -1,11 +1,5 @@
 module.exports = {
-	'back' : function(args){
-		this.render('back.html')
-	}
-	, 'front' : function(args){
-		this.render('front.html')
-	}
-	, 'canvas' : function(args){
+	'index' : function(args){
 		this.render('canvas.html')
 	}
 }
