a7157890db0a36e4c73e258cc8d72a5d677abf62
xh1021 <xh1021@vip.qq.com>
2016-07-17 07:38:38 +0800

    view

---
+++ controller/back.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'index' : function(args){
+		this.render('back.html')
+	}
+}
---
+++ controller/front.js
@@ -0,0 +1,11 @@
+module.exports = {
+	'back' : function(args){
+		this.render('back.html')
+	}
+	, 'front' : function(args){
+		this.render('front.html')
+	}
+	, 'canvas' : function(args){
+		this.render('canvas.html')
+	}
+}
