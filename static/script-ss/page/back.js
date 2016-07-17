34e92751e8fcc3593abdfd1ad6f11914d89620d2
xh1021 <xh1021@vip.qq.com>
2016-07-17 08:15:33 +0800

    pages

--- static/script-ss/page/back.js
+++ static/script-ss/page/back.js
@@ -2,12 +2,12 @@ Vue.config.debug = true
 
 var app = require('back/app.vue')
 
-var pages = require('data/pages')
-
 /*
+var pages = require('data/pages')
+*/
+
 var pageConfigStr = localStorage.getItem("pageConfig")
 var pages = [JSON.parse(pageConfigStr)]
-*/
 
 var vm = new Vue({
   el: 'body'
